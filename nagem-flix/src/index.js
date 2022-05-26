1de5b29c9cab3f97fe72b8904607d0e0eca8ecfb
Diechabral <th3d1eg0master@gmail.com>
2022-05-26 19:42:53 -0300

    paginação de login

--- nagem-flix/src/index.js
+++ nagem-flix/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import Home from './pages/Home';
-
+import Login from './pages/Login';
+import "./pages/Login/style.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Home />
+    <Login />
 );
 
 
---
+++ nagem-flix/src/pages/Login/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+const Login = () => {
+
+    return (
+        <div className="container-fluid">
+            <header className="row">
+                <img src={require('../../assets/20210830151858.png')}/>
+            </header>
+            <div id="caixa_login" className="col-4 offset-4"></div>
+            <h1 className="text=white">Entrar</h1>
+            <br />
+            <form>
+                <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email ou Número de telefone" />
+                <br />
+                <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Senha" />
+                <br />
+                <button className="btn btn-lg btn-block btn-danger">Entrar</button>
+                <div className="row mt-4">
+                    <div className="col text-muted">
+                        <input type="checkbox" /> Lembrar de mim.
+                        <a href="#" className="text-muted">
+                            Precisa de ajuda?
+                        </a>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+
+    )
+}
+
+export default Login
