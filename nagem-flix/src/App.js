fbd726e3fd68dc8d62c802385d20153756b23f93
Diechabral <th3d1eg0master@gmail.com>
2022-05-27 13:58:35 -0300

    update

--- nagem-flix/src/App.js
+++ nagem-flix/src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Routes, Link, } from "react-router-dom";
 import Catalogo from './pages/catalogo'
 import Login from './pages/login.js';
-import Sobre from './pages/sobre.js';
 import { Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,12 +13,10 @@ function App() {
                 <Nav variant="tabs">
                     <Nav.Link as={Link} to="/">Faça login </Nav.Link>
                     <Nav.Link as={Link} to="/catalogo">Catálogo</Nav.Link>
-                    <Nav.Link as={Link} to="/sobre">Sobre</Nav.Link>
                 </Nav>
                 <Routes>
                     <Route path="/" element={<Login />}></Route>
                     <Route path="/catalogo" element={<Catalogo />}></Route>
-                    <Route path="/sobre.js" element={<Sobre />}></Route>
                 </Routes>
             </Router>
 
